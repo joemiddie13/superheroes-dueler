e61fa418b05ff6a76183fffef1e35ca46cf70391
joemiddie13 <joemiddie13@gmail.com>
2023-10-30 15:13:11 -0700

    created dog class

---
+++ my_dogs.py
@@ -0,0 +1,13 @@
+import dog
+
+# Dog 1
+my_dog = dog.Dog("Rocko", "SuperDog")
+my_dog.bark()
+
+# Dog 2
+dad_dog = dog.Dog("Holly", "German Short Haired Pointer")
+dad_dog.sit()
+
+# Dog 3
+mom_dog = dog.Dog("Mango", "Mixed SuperDog")
+mom_dog.roll_over()
